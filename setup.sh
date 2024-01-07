7dba0883501396e50ed3ae478751fb5efb3ac1c2
Laith Yousif <laith@blabby.me>
2024-01-06 23:34:41 -0500

    update oh my posh folder

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-curl -s https://ohmyposh.dev/install.sh | bash -s
+curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/bin
 
 # powerline fonts for zsh agnoster theme
 git clone https://github.com/powerline/fonts.git
