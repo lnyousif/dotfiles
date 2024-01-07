653dd6e6910cd59584407b1529512a3cd23c69f0
Laith Yousif <laith@blabby.me>
2024-01-06 23:10:53 -0500

    Add Setup Scrpts

---
+++ setup.sh
@@ -0,0 +1,9 @@
+curl -s https://ohmyposh.dev/install.sh | bash -s
+
+# powerline fonts for zsh agnoster theme
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+cd .. && rm -rf fonts
+
+eval "$(oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/jandedobbeleer.omp.json)"
