e41640e928ea52fc64fdd12d49eea1d2c8a369ee
Laith Yousif <laith@blabby.me>
2024-01-08 10:03:31 -0500

    Resetting the files

--- setup.sh
+++ setup.sh
@@ -1,9 +1,1 @@
-curl -s https://ohmyposh.dev/install.sh | bash -s
-
-# powerline fonts for zsh agnoster theme
-git clone https://github.com/powerline/fonts.git
-cd fonts
-./install.sh
-cd .. && rm -rf fonts
-
-eval "$(oh-my-posh init zsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/jandedobbeleer.omp.json')"
+#to be filled 
