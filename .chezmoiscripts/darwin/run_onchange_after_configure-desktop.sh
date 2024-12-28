d50cf3e68d8c80a48c85e65e86d953eeb6f55b27
Laith Yousif <laith@MacBook-Pro.local>
2024-12-28 15:06:39 -0500

    Improving the Mac support

--- .chezmoiscripts/darwin/run_onchange_after_configure-desktop.sh
+++ .chezmoiscripts/darwin/run_onchange_after_configure-desktop.sh
@@ -2,4 +2,4 @@
 
 set -eufo pipefail
 
-osascript -e 'tell application "System Events" to tell every desktop to set picture to "/System/Library/Desktop Pictures/Solid Colors/Space Gray Pro.png" as POSIX file'
+osascript -e 'tell application "System Events" to tell every desktop to set picture to "/System/Library/Desktop Pictures/Solid Colors/Electric Blue.png" as POSIX file'
