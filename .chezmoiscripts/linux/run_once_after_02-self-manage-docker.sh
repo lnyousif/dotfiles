e786f8f5aab0380c4a35bcb0b01000a583a54780
Laith Yousif <Laith@tmobi.link>
2024-12-30 11:24:46 -0500

    Cleaning up

--- .chezmoiscripts/linux/run_once_after_02-self-manage-docker.sh
+++ .chezmoiscripts/linux/run_once_after_02-self-manage-docker.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-export PATH="${HOME}/.local/bin:${HOME}/bin:${PATH}"
+#export PATH="${HOME}/.local/bin:${HOME}/bin:${PATH}"
 
 # Allow management of Docker as a non-root user
-sudo groupadd docker -f
-sudo usermod -aG docker "${USER}"
-sudo systemctl enable docker.socket
+#sudo groupadd docker -f
+#sudo usermod -aG docker "${USER}"
+#sudo systemctl enable docker.socket
 
-echo "Docker is now managable by ${USER}"
-echo "Please reboot once for changes to take effect"
+#echo "Docker is now managable by ${USER}"
+#echo "Please reboot once for changes to take effect"
