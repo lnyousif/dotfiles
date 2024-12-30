1cde53b623548de13b1e5b9f0d3080d3bcde013b
Laith Yousif <Laith@tmobi.link>
2024-12-30 11:24:46 -0500

    Cleaning up

--- .chezmoiscripts/linux/run_once_before_01-configure-repos-and-executables.sh
+++ .chezmoiscripts/linux/run_once_before_01-configure-repos-and-executables.sh
@@ -19,43 +19,6 @@ if command -v apt &>/dev/null; then
   sudo nala fetch --auto -y --https-only --non-free
   sudo nala install --update -y curl wget git ca-certificates libfuse2 gnupg2
 
-  # Add Repositories
-  echo "Setting up repositories"
-
-  ## KeePassXC
-  sudo add-apt-repository ppa:phoerious/keepassxc -y
-  echo "${LINE}"
-
-  ## Waydroid
-  curl https://repo.waydro.id | sudo bash
-  echo "${LINE}"
-
-  ## Docker Engine
-
-  sudo nala remove docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc
-  sudo install -m 0755 -d /etc/apt/keyrings
-  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-  sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-  ### Add the repository to Apt sources:
-  # shellcheck disable=SC1091
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "${UBUNTU_CODENAME}") stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-
-  ## Fastfetch
-  sudo add-apt-repository ppa:zhangsongcui3371/fastfetch -y
-
-  ## GitHub CLI
-  sudo mkdir -p -m 755 /etc/apt/keyrings
-  wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null
-  sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null
-
-  ## OneDrive CLI
-  sudo nala remove onedrive -y
-  sudo add-apt-repository --remove ppa:yann1ck/onedrive &>/dev/null 2>&1 || true
-  wget -qO - https://download.opensuse.org/repositories/home:/npreining:/debian-ubuntu-onedrive/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /usr/share/keyrings/obs-onedrive.gpg >/dev/null
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/obs-onedrive.gpg] https://download.opensuse.org/repositories/home:/npreining:/debian-ubuntu-onedrive/xUbuntu_22.04/ ./" | sudo tee /etc/apt/sources.list.d/onedrive.list
-
 elif command -v dnf &>/dev/null; then
 
   # Install dnf5 for faster downloads
@@ -86,14 +49,6 @@ elif command -v dnf &>/dev/null; then
   echo "Setting up repositories"
   sudo dnf -y install dnf-plugins-core
 
-  ## Docker Engine
-  sudo dnf remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine
-  #sudo dnf config-manager --add-repo=https://download.docker.com/linux/fedora/docker-ce.repo
-
-  ## RPM Fusion Free & Non-free repos
-  #fedora_version=$(rpm -E %fedora)
-  #sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"${fedora_version}".noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"${fedora_version}".noarch.rpm -y
-  #sudo dnf config-manager --enable fedora-cisco-openh264 -y
 
   trap 'rm -fr /tmp/dra' EXIT
   ## Download dra executable
