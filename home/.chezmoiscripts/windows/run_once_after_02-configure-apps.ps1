e6889121e483c7afde93ce05aeb88ea88cfb29f9
Laith Yousif <Laith@tmobi.link>
2025-04-27 14:35:36 -0400

    improved windows implementation

--- home/.chezmoiscripts/windows/run_once_after_02-configure-apps.ps1
+++ home/.chezmoiscripts/windows/run_once_after_02-configure-apps.ps1
@@ -1,9 +1,4 @@
-# Install ggshield global precommit hook
-ggshield install -m global
 
 # Install Nerd font
 oh-my-posh font install meslo
 oh-my-posh enable autoupgrade
-
-# Set gh as default git credentials helper
-gh auth setup-git
