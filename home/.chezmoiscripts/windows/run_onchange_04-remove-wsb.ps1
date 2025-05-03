33e3a4c7c358fbd334b5847988454f5d84449e9d
Laith Yousif <Laith@tmobi.link>
2025-05-02 20:37:03 -0400

    refactor(starship): geting starship to work on windows powershell

--- home/.chezmoiscripts/windows/run_onchange_04-remove-wsb.ps1
+++ home/.chezmoiscripts/windows/run_onchange_04-remove-wsb.ps1
@@ -1,2 +1,5 @@
 New-Item "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Force
+
 New-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name "EnableDynamicContentInWSB" -PropertyType DWORD -Value 0
+
+Add-Content -Path $PROFILE -Value 'Invoke-Expression (&starship init powershell)'
