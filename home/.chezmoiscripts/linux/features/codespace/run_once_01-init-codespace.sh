56617cda861b5bdf30d92943be8cb13008de7886
Laith Yousif <Laith@tmobi.link>
2025-05-10 18:25:33 -0400

    refactor(codespace): switching from setup to a feature

---
+++ home/.chezmoiscripts/linux/features/codespace/run_once_01-init-codespace.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Display a clear and colorful terminal message
+echo -e "\033[1;36m========================================\033[0m"
+echo -e "\033[1;32m🚀 Welcome to your CodeSpace Environment!\033[0m"
+echo -e "\033[1;33m🔧 Setting up your environment using DotFiles...\033[0m"
+echo -e "\033[1;36m========================================\033[0m"
