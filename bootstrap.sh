bded5e28d4faf0e97090576aa283a6db39c0a8ac
Laith Yousif <Laith@tmobi.link>
2025-06-06 10:47:27 -0400

    adding bash info

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,6 +23,19 @@ fi
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
+
+# Set environment variables for chezmoi configuration
+export CHEZMOI_NAME="Laith Yousif"
+export CHEZMOI_EMAIL="Laith@tmobi.link"
+export CHEZMOI_SETUP_TYPE="basic"  # Options: basic, kids, development, work
+export CHEZMOI_ENCRYPTION=false     # Set to true to enable encryption
+export CHEZMOI_PASS_MANAGER=false    # Set to true to enable password manager
+export CHEZMOI_NETWORK_HUB=false     # Set to true if this is a network hub
+export CHEZMOI_NETWORK_NODE=false    # Set to true if this is a node machine
+
+echo "Environment variables for chezmoi have been set."
+
+
 set -- init --apply --source="${script_dir}"
 
 echo "Running 'chezmoi $*'" >&2
