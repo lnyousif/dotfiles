9f5808aabca351d17868ed60aee7ade512cf536f
Laith Yousif <Laith@tmobi.link>
2025-06-06 16:57:22 -0400

    updated info

---
+++ pre-init.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+# Set environment variables for chezmoi configuration
+export CHEZMOI_NAME="Laith Yousif"
+export CHEZMOI_EMAIL="Laith@tmobi.link"
+export CHEZMOI_SETUP_TYPE="basic"  # Options: basic, kids, development, work
+export CHEZMOI_ENCRYPTION=false     # Set to true to enable encryption
+export CHEZMOI_PASS_MANAGER=false    # Set to true to enable password manager
+export CHEZMOI_NETWORK_HUB=false     # Set to true if this is a network hub
+export CHEZMOI_NETWORK_NODE=false    # Set to true if this is a node machine
+
+echo "Environment variables for chezmoi have been set."
