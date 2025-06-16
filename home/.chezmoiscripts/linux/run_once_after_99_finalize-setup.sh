c4d8563c227bf8a5044162cb67f5df8817afb53c
Laith Yousif <Laith@tmobi.link>
2025-06-16 17:58:16 -0400

    refactor(ssh): update key setup script and rename templates

---
+++ home/.chezmoiscripts/linux/run_once_after_99_finalize-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "====================================================="
+echo "🧹 Almost done! Performing final Operations..."
+echo "====================================================="
+
+source ~/.bashrc  # For bash
+source ~/.zshrc   # For zsh
+
+echo "✅ Operations complete!"
