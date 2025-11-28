5759eb7cf35762acbc523ac133d10ec49827372d
Laith Yousif <Laith@tmobi.link>
2025-11-28 17:50:35 -0500

    feat: enable Podman socket for user in final setup script

--- home/.chezmoiscripts/linux/run_once_after_99_finalize-setup.sh
+++ home/.chezmoiscripts/linux/run_once_after_99_finalize-setup.sh
@@ -4,6 +4,9 @@ echo "====================================================="
 echo "🧹 Almost done! Performing final Operations..."
 echo "====================================================="
 
+# Enable Podman socket for user
+systemctl --user enable podman.socket
+
 source ~/.bashrc  # For bash
 source ~/.zshrc   # For zsh
 
