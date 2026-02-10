4b5a0776dd30e33c503a4c2639c3d79a2e6e0474
Laith Yousif <Laith@tmobi.link>
2026-02-10 12:18:50 -0500

    Clean up

--- home/.chezmoiscripts/windows/10_setups/base/run_onchange_02_remove-wsb.ps1
+++
@@ -1,5 +0,0 @@
-New-Item "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Force
-
-New-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name "EnableDynamicContentInWSB" -PropertyType DWORD -Value 0
-
-Add-Content -Path $PROFILE -Value 'Invoke-Expression (&starship init powershell)'
